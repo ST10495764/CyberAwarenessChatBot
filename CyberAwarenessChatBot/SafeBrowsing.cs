6b80f7b75c650570d8cb83e67024b3e51d7375e0
ST10495764 <ST10495764@VCCONNECT.EDU.ZA>
2026-03-31 15:42:29 +0200

    added content about safe browsing for answers

--- CyberAwarenessChatBot/SafeBrowsing.cs
+++ CyberAwarenessChatBot/SafeBrowsing.cs
@@ -10,17 +10,40 @@ namespace CyberAwarenessChatBot
 
         public override void Definition()
         {
-            Console.WriteLine("safe browsing definition");
+            Console.WriteLine(@"
+Safe browsing means using the internet in a way that protects your devices and personal information from 
+threats like malware, scams, and unsafe websites.");
         }
 
         public override void Identify()
         {
-            Console.WriteLine("identifying if you are safe browsing");
+            Console.WriteLine(@"
+How to Identify Unsafe Websites:
++URLs that start with http:// instead of https://
++Poor website design or lots of pop-ups
++Requests for unnecessary personal information
++Browser warnings about unsafe sites
++Fake download buttons");
         }
 
         public override void BestPracticeAndMitigations()
         {
-            Console.WriteLine("best practice for safe browsing");
+            Console.WriteLine(@"
+Best Practices:
++Only visit trusted and secure websites (HTTPS)
++Keep your browser and software up to date
++Avoid clicking on suspicious ads or pop-ups
++Install reputable antivirus/security software
++Log out of accounts when using shared devices
++Be cautious when using public Wi-Fi (use a VPN if possible)");
+        }
+
+        public void ShowAll()
+        {
+            Definition();
+            Identify();
+            BestPracticeAndMitigations();
+
         }
     }
 }
